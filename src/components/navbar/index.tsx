57823e6385c1d4602e37854119bd99865481b186
bin <tinnt0504@gmail.com>
2024-06-20 09:38:07 +0700

    feet: change pass

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -6,12 +6,51 @@ import {
   MenuItems,
   Transition,
 } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useAuth } from "@providers/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import DetailModal from "@components/modal";
+import { useRouter } from "next/navigation";
+import { useLoading } from "@providers/loading";
+import { restApiBase } from "@libs/restApi";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 // Define the User type based on your application's user structure
 const Navbar = () => {
   const { activeItem, logout, user } = useAuth();
+  const { setLoading }: any = useLoading();
+  const router = useRouter();
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [oldPassword, setOldPassword] = useState<string>();
+  const [newPassword, setNewPassword] = useState<string>();
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleCloseDetailModal = () => {
+    setIsOpenModal(false);
+  };
+  const handleOpenDetailModal = () => {
+    setIsOpenModal(true);
+  };
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const handleChangePassword = async (oldPass: string, newPass: string) => {
+    try {
+      const result = await restApiBase({oldPassword: oldPass, newPassword: newPass}, 'api/auth/change-password', 'PUT');  
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+  const handleSubmit = async () => {
+    setLoading(true);
+    await handleChangePassword(oldPassword as string, newPassword as string);
+    setIsOpenModal(false);
+    logout();
+    router.push("/login");
+  };
+
   return (
     <>
       <Disclosure as="nav" className="bg-nashtech text-white">
@@ -36,7 +75,7 @@ const Navbar = () => {
                   <MenuItems className="absolute right-0 mt-2 w-40 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="p-3 flex justify-center flex-col">
                       <div className="hover:bg-nashtech hover:text-white text-black text-sm rounded px-2 mb-2">
-                        <a href="#">Change Password</a>
+                        <a href="#" onClick={handleOpenDetailModal}>Change Password</a>
                       </div>
                       <div className="hover:bg-nashtech hover:text-white text-black text-sm rounded px-2">
                         <a href="#" onClick={logout}>
@@ -51,6 +90,86 @@ const Navbar = () => {
           </div>
         </div>
       </Disclosure>
+      <DetailModal
+        isOpen={isOpenModal}
+        onClose={handleCloseDetailModal}
+        title="Change Password">
+        <div>
+          <form
+            className="w-full p-5"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}>
+            <div className="md:flex md:items-center mb-3 relative">
+              <div className="md:w-1/3">
+                <label className="block text-gray-700 mb-1 md:mb-0 pr-4">
+                  Old Password
+                </label>
+              </div>
+              <div className="md:w-2/3 relative">
+                <input
+                  className="border border-black rounded w-full py-1 px-4"
+                  id="inline-password"
+                  name="oldPassword"
+                  type={showPassword ? "text" : "password"}
+                  value={oldPassword}
+                  onChange={(e) => {
+                    setOldPassword(e.target.value), setErrorMsg("");
+                  }}
+                />
+                <button
+                  type="button"
+                  onClick={toggleShowPassword}
+                  className="absolute right-0 top-0 mt-1 mr-2 text-gray-600 focus:outline-none">
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                </button>
+              </div>
+            </div>
+            <div className="md:flex md:items-center mb-3 relative">
+              <div className="md:w-1/3">
+                <label className="block text-gray-700 mb-1 md:mb-0 pr-4">
+                  New Password
+                </label>
+              </div>
+              <div className="md:w-2/3 relative">
+                <input
+                  className="border border-black rounded w-full py-1 px-4"
+                  id="inline-password"
+                  name="newPassword"
+                  type={showPassword ? "text" : "password"}
+                  value={newPassword}
+                  onChange={(e) => {
+                    setNewPassword(e.target.value), setErrorMsg("");
+                  }}
+                />
+                <button
+                  type="button"
+                  onClick={toggleShowPassword}
+                  className="absolute right-0 top-0 mt-1 mr-2 text-gray-600 focus:outline-none">
+                  <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+                </button>
+              </div>
+            </div>
+            <div className="h-5">
+              <span className="text-nashtech text-xs italic">{errorMsg}</span>
+            </div>
+
+            <div className="flex flex-row-reverse mt-2">
+              <button
+                className={`bg-nashtech text-white py-1 px-3 rounded ${
+                  !oldPassword && !newPassword
+                    ? "opacity-50 cursor-not-allowed"
+                    : "hover:opacity-75"
+                }`}
+                disabled={!oldPassword && !newPassword}
+                type="submit">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </DetailModal>
     </>
   );
 };
