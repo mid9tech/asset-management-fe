9e72f237f0707eb2a3fe7fc17229b561b4607f4b
bin <tinnt0504@gmail.com>
2024-06-25 17:44:40 +0700

    fix: refresh token + / path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,19 @@ const nextConfig = {
   env: {
     PORT: process.env.PORT,
     NEXT_PUBLIC_URL_SERVER: process.env.NEXT_PUBLIC_URL_SERVER,
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: `/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_URL_SERVER}/api/:path*`,
+      },
+      {
+        source: `/graphql/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_URL_SERVER}/graphql/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
