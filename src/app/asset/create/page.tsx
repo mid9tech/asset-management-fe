4f7c4541bad703d253c93e0b95117fff4fa2daa6
Nguyen Phu Y <ynguyenphu@nashtechrookiesb724.onmicrosoft.com>
2024-06-27 04:48:33 +0000

    Merged PR 133: [FIX] 367 Cancel Button Click
    Related work items: #367

--- src/app/asset/create/page.tsx
+++ src/app/asset/create/page.tsx
@@ -49,34 +49,6 @@ const CreateAsset = () => {
                 <h1 className="text-nashtech font-semibold mb-5">Create New Asset</h1>
                 <FormCreateAsset/>
             </div>
-            <DetailModal
-                isOpen={showModalCancel}
-                onClose={handleCloseCancelModal}
-                title="Are you sure">
-                <div className="bg-white sm:p-6 sm:pb-4">
-                    <div className="sm:flex sm:items-start">
-                        <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                            <p className="text-md text-gray-500">
-                                Do you want to cancel changes?
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-gray-50 sm:flex sm:flex-row-reverse gap-4">
-                    <Button
-                        type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                        onClick={() => setShowModalCancel(false)}>
-                        Cancel
-                    </Button>
-                    <Button
-                        type="button"
-                        className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                        onClick={handleDiscard}>
-                        Discard
-                    </Button>
-                </div>
-            </DetailModal>
         </>
     );
 };
