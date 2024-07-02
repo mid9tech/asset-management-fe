78efaa7c4f566b3ced4a58c0271a62f4870e29a5
bin <tinnt0504@gmail.com>
2024-07-02 09:14:06 +0700

    fix: layout for edit

--- src/app/assignment/components/form/editForm.tsx
+++ src/app/assignment/components/form/editForm.tsx
@@ -182,6 +182,7 @@ const EditForm: FC<FormProps> = (props) => {
           <label>Note</label>
           <textarea
             onChange={(e) => setNoteValue(e.target.value)}
+            value={data?.note || ""}
             id="note-assignment"
             rows={5}
             className="flex h-auto w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
