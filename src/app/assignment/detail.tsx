2c6e971d7b5d99b12129e24b87ef4ddce9dc64ad
Nguyen Phu Y <ynguyenphu@nashtechrookiesb724.onmicrosoft.com>
2024-07-05 02:48:29 +0000

    Merged PR 238: [FIX] 647 Message joined date is not correct
    Related work items: #647

--- src/app/assignment/detail.tsx
+++ src/app/assignment/detail.tsx
@@ -17,6 +17,28 @@ export default function DetailAssignment({
       isOpen={showModalDetailUser}
       onClose={handleCloseDetailModal}
       title="Detailed Assignment Information">
+      {/* <div className="grid grid-cols-2 gap-4">
+        <div className="flex flex-col gap-2">
+          <div className="text-sm w-40">Asset Code:</div>
+          <div className="text-sm w-40">Asset Name:</div>
+          <div className="text-sm w-40">Specification:</div>
+          <div className="text-sm w-40">Assigned to:</div>
+          <div className="text-sm w-40">Assigned by:</div>
+          <div className="text-sm w-40">Assigned date:</div>
+          <div className="text-sm w-40">State:</div>
+          <div className="text-sm w-40">Note:</div>
+        </div>
+        <div className="flex flex-col gap-2">
+        <div className="text-sm w-40">{data.assetCode}</div>
+          <div className="text-sm w-40">{data.assetName}</div>
+          <div className="text-sm w-40">{data.asset?.specification}</div>
+          <div className="text-sm w-40">{data.assignedToUsername}</div>
+          <div className="text-sm w-40">{data.assignedByUsername}</div>
+          <div className="text-sm w-40">{data.assignedDate}</div>
+          <div className="text-sm w-40">{data.state}</div>
+          <div className="text-sm w-40">{data.note}</div>
+        </div>
+      </div> */}
       <div className="text-gray">
         <div className="flex flex-row mb-2">
           <span className="text-sm w-40">Asset Code:</span>{" "}
