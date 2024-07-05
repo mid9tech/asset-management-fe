4b7822a9aa3c40a5b702123f88235e9f2baf5809
Nguyen Phu Y <ynguyenphu@nashtechrookiesb724.onmicrosoft.com>
2024-07-05 05:45:20 +0000

    Merged PR 241: [FIX] 661 Confirmation home user
    Related work items: #661

--- src/app/home/components/table/homeList.tsx
+++ src/app/home/components/table/homeList.tsx
@@ -221,14 +221,14 @@ const HomeList = <T extends {}>({
             className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
             onClick={handleCloseCancelModal}
           >
-            Cancel
+            No
           </Button>
           <Button
             type="button"
             className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
             onClick={handleConfirm}
           >
-            Confirm
+            Yes
           </Button>
         </div>
       </DetailModal>
