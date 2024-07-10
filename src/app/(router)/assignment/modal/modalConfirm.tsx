b79058667ca2b62c4e76ce7b3beb778788f2ee06
bin <tinnt0504@gmail.com>
2024-07-10 15:42:32 +0700

    style: outline button

--- src/app/(router)/assignment/modal/modalConfirm.tsx
+++ src/app/(router)/assignment/modal/modalConfirm.tsx
@@ -28,6 +28,7 @@ const ModalConfirmAssignment: FC<ModalConfirmProps> = (props) => {
       <div className="bg-gray-50 sm:flex sm:flex-row-reverse gap-4">
         <Button
           type="button"
+          variant="outline"
           className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
           onClick={() => setShowModalConfirm(false)}>
           Cancel
