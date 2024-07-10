ef46f767b67b8afe7b705d675cf7f813c9bc77ea
bin <tinnt0504@gmail.com>
2024-07-10 16:06:34 +0700

    style: variant button

--- src/app/(router)/request-returning/components/modal/complete.tsx
+++ src/app/(router)/request-returning/components/modal/complete.tsx
@@ -26,7 +26,8 @@ const ModalCompleteRequestReturn: FC<ModalConfirmProps> = (props) => {
       <div className="bg-gray-50 sm:flex sm:flex-row-reverse gap-4">
         <Button
           type="button"
-          className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+          variant="outline"
+          className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
           onClick={() => setShowModalConfirm(false)}
         >
           No
--- src/app/(router)/request-returning/components/model/cancel.tsx
+++ src/app/(router)/request-returning/components/model/cancel.tsx
@@ -20,6 +20,7 @@ const ModalCancelRequestReturn: FC<ModalConfirmProps> = (props) => {
       <div className="bg-gray-50 sm:flex sm:flex-row-reverse gap-4">
         <Button
           type="button"
+          variant="outline"
           className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm text-gray shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
           onClick={() => setShowModalConfirm(false)}
         >
